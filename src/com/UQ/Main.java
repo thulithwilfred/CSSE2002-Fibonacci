a2fa32e930846342bddeaeab32a5a0c61bcba3aa
Thulith W Mallawa <thulith.mallawa@uqconnect.edu.au>
2021-01-20 19:03:27 +1000

    /* Build Complete */ Added alt recusion method.

--- src/com/UQ/Main.java
+++ src/com/UQ/Main.java
@@ -21,13 +21,24 @@ public class Main {
 
             int nTh = usrIn.nextInt();
 
+            /* Use Either Of the below, they use different approaches */
             System.out.println("F(" + nTh + ") is: " + fib(nTh) + "\n");
+
+            //System.out.println("F(" + nTh + ") is: " + fibRec(nTh - 1 ) + "\n");
 
         }
     }
 
-    public static int fib(int n) {
+    public static int fibRec(int n) {
+    /* Implemented using recursion */
+        if (n < 2) {
+            return 1;
+        }
+        return fibRec(n - 1) + fibRec(n - 2);
+    }
 
+    public static int fib(int n) {
+        /* Implemented using equation */
         int fibVal = (n-1)+(n-2);
         double Phi = ((1 + Math.sqrt(5))/2);
         double phi = ((1 - Math.sqrt(5))/2);
